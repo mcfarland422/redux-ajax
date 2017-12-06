5ff8571331283bb1dfe5c2158c1da0ff1cb37ce8
Michael McFarland <gardengrove422@gmail.com>
2017-12-06 13:18:53 -0500

    third'ish

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
+// In order to wire up a redux React app we need the following:
+// 1. Provider component. THis comes from react-redux
+// 2. createStore. This comes from redux.
+// 3. reducers to give to createStore
+// 4. Make a single rootReducer (3) to import our individual reducers
+// 5. Make at least one indidivual reducer to import into root (4)
+// 6. Create theStore (2) with the reducers (3 -> 4 -> 5)
+// 7. Put the Provider Component (1) with a prop of store with the store (6) around <App />
+// 8. If we need redux middleware, we need to import it from redux (applyMiddleWare)
+// 9. import redux-promise so that if an action needs to return a promise/async
+// the middleware will tell REdux to sha-lax... I'm coming.
+// 10. When we make the store, kind of like connect, we apply middleware first
+// then, we hand it the createStore
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// redux stuff
+import { Provider } from 'react-redux'; //Item 1
+import { createStore, applyMiddleware } from 'redux'; // Item 2 & 8
+import reducers from './reducers/index' //item 3 & 4 & 5
+import reduxPromise from 'redux-promise';
+
+// const theStore = createStore(reducers); //item 6 - removving because we have middleware
+// const theStoreWithMiddleWare = applyMiddleware(reduxPromise)(createStore)(reducers);
+const middleWare = applyMiddleware(reduxPromise); //returns a function
+// console.log(middleWare)
+const theStore = middleWare(createStore); //returns another function
+// console.log(theStore);
+const theStoreWithReducers = theStore(reducers)
+
+ReactDOM.render(
+	<Provider store={theStoreWithReducers}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
+
